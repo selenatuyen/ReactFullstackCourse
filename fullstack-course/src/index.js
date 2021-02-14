1dda4ac722c03f2c9b13070eaba4ee597b61ef17
selenatuyen <selenaltuyen@gmail.com>
2021-02-14 12:45:30 -0800

    Excercise 1.3

--- fullstack-course/src/index.js
+++ fullstack-course/src/index.js
@@ -8,25 +8,23 @@ import Total from './Components/Total';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content parts={parts}></Content>
+      <Content part1={part1} part2={part2} part3={part3}></Content>
       <Total total={part1.exercises + part2.exercises + part3.exercises}></Total>
     </div>
   )
