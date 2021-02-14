b9ae3c9f5d944ee6f6222b86d2a36a16a2fe4461
selenatuyen <selenaltuyen@gmail.com>
2021-02-14 12:44:56 -0800

    Excercise 1.3

--- fullstack-course/src/Components/Content.js
+++ fullstack-course/src/Components/Content.js
@@ -4,9 +4,9 @@ import Part from './Part';
 const Content = (props) => {
     return (
         <div>
-            <Part partNumber={props.part1} exerciseNumber={props.exercises1}></Part>
-            <Part partNumber={props.part2} exerciseNumber={props.exercises2}></Part>
-            <Part partNumber={props.part3} exerciseNumber={props.exercises3}></Part>
+            <Part partNumber={props.part1.name} exerciseNumber={props.part1.exercises}></Part>
+            <Part partNumber={props.part2.name} exerciseNumber={props.part2.exercises}></Part>
+            <Part partNumber={props.part3.name} exerciseNumber={props.part3.exercises}></Part>
         </div>
     )
 }
--- fullstack-course/src/index.js
+++ fullstack-course/src/index.js
@@ -8,18 +8,26 @@ import Total from './Components/Total';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Content>
-      <Total total={exercises1 + exercises2 + exercises3}></Total>
+      <Content parts={parts}></Content>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}></Total>
     </div>
   )
 }
