14a3435622fd90db19487eb9e265f3e40a5ba0ac
selenatuyen <selenaltuyen@gmail.com>
2021-02-21 17:13:05 -0800

    Exercise 1.13, anecdotes step 2

--- fullstack-course/part1/anecdotes/src/index.js
+++ fullstack-course/part1/anecdotes/src/index.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0));
 
   const getRandomQuote = () => {
     return setSelected(Math.floor(Math.random() * Math.floor(anecdotes.length)));
   }
 
+  const addVote = () => {
+    const pointsCopy = [...points];
+    pointsCopy[selected] += 1;
+    return setPoints(pointsCopy);
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={addVote}>Vote</button>
       <button onClick={getRandomQuote}>Next Anecdote</button>
     </div>
   )
@@ -24,6 +33,7 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
