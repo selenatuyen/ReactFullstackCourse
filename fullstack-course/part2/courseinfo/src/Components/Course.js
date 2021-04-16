e11844dbd83abc32f4a544de941aa07ace54aef6
Selena Tuyen <stuyen@firstam.com>
2021-04-16 15:18:42 -0700

    Complete 2.1 and 2.2

---
+++ fullstack-course/part2/courseinfo/src/Components/Course.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Header = ({name}) => {
+    return <h1>{name}</h1>
+};
+
+const Part = ({part}) => {
+    return <p key={part.id}>{part.name} {part.exercises}</p>
+};
+
+const Content = ({parts}) => {
+    return <div>
+                {parts.map((part) => 
+                    <Part part={part}/>
+                )}
+            </div>
+};
+
+const Total = ({parts}) =>{
+    let total = parts.reduce((sum, part) => {return sum + part.exercises}, 0);
+    return <p>Total of {total} exercises</p>
+};
+
+
+const Course = ({course}) => {
+    return ( 
+        <div>
+            <Header name={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+}
+
+export default Course;
