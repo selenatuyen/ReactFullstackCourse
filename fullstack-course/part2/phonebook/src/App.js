cd867cbe5ba546ccc0a9bb8683f4950682534889
selenatuyen <selenaltuyen@gmail.com>
2021-05-23 15:44:13 -0700

    Part 2.11

--- fullstack-course/part2/phonebook/src/App.js
+++ fullstack-course/part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from "./Components/Filter";
 import PersonForm from "./Components/PersonForm";
 import Persons from "./Components/Persons";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filterSearch, setFilterSearch] = useState('');
@@ -42,6 +38,15 @@ const App = () => {
       setPersons(persons.concat(person));
     }
   }
+
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        // console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
